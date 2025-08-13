733e55cdeb6e70f18b8052f711f9625e0c7313c0
Vinicius Dias <viniciuscorp07@gmail.com>
2025-08-13 17:52:40 -0300

    Primeiras aplicações de Orientação a Objeto

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 from models.restaurante import Restaurante
+from models.cardapio.bebidas import Bebidas
+from models.cardapio.prato import Prato
+
 rest_1 = Restaurante('surf dog', 'executivo')
 rest_2 = Restaurante('burgman', 'bar')
 rest_3 = Restaurante('Sr miag', 'japones')
 
-rest_1.altelnar_estado()
-rest_1.receber_avaliacao('Vinicius', 8)
-rest_1.receber_avaliacao('Amanda', 4)
+bebida_suco = Bebidas('Laranja', 11.90, '400 ml')
+hamburger_prato = Prato('Hamburger master', 45.8, 'O maior burger da casa')
 
 def main():
-    Restaurante.listar_restaurantes()
+    pass
 
 if __name__ == '__main__':
     main()
